f3354092d581e2853f0f3f5599b93e9ddcf4b64c
Freddie Fujiwara <github@ze.gs>
2013-04-06 16:24:11 +0900

    final

--- index.php
+++ index.php
@@ -4,13 +4,22 @@ include 'cloudinary_php/src/Uploader.php';
 if (file_exists('settings.php')) {
   include 'settings.php';
 }
-$hash= hexdec(substr (hash('md5',basename($_FILES['userfile']['name'])),0,4));
 $uploaddir = '/tmp/'.time();
 $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+$hash= 0;
 move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile);
+$handle = @fopen($uploadfile, "r");
+if ($handle) {
+    while (!feof($handle)) {
+        $hash = hexdec(bin2hex(fread ($handle , 4 )));
+    }
+    fclose($handle);
+
+}
 $result = \Cloudinary\Uploader::upload($uploadfile,array("width" => 90, "height" => 90, "crop" => "thumb", "gravity" => "face"));
 unlink($uploadfile);
 
-$key=$keys[$hash % count($keys)-1];
+$index=$hash % count($keys);
+$key=$keys[$index < 0 ? 0 : $index];
 header("HTTP/1.1 301 Moved Permanently");
 header("Location: http://afthon.github.io/apr06/result.html?url=".urlencode($result['url'])."&keyword=".urlencode($key));
